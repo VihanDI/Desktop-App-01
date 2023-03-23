1940729d5e9c9eb333961eb71ad9652e008c8b8e
Inupa Vihan <inupavihan9@gmail.com>
2023-03-24 01:08:55 +0530

    Phase 01 Completed
    Completed the development phase 01
    -Main window
    -Add new student window
    -Database added

---
+++ Desktop_App_01/Views/AddStudentWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Desktop_App_01.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Desktop_App_01.Views
+{
+    /// <summary>
+    /// Interaction logic for AddStudentWindow.xaml
+    /// </summary>
+    public partial class AddStudentWindow : Window
+    {
+        public AddStudentWindow()
+        {
+            InitializeComponent();
+            DataContext = new AddStudentWindowVM();
+        }
+
+        private void Button_Enter(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+
+        private void Button_Cancel(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
