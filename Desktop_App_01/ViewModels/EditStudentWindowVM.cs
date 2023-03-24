88d03412b9c7ea1a640c69fa6e0ae2936a726a7b
Inupa Vihan <inupavihan9@gmail.com>
2023-03-24 20:39:58 +0530

    Phase 02 Completed
    Completed the development phase 02
    -Edit student operation
    -Delete student operation

---
+++ Desktop_App_01/ViewModels/EditStudentWindowVM.cs
@@ -0,0 +1,69 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Desktop_App_01.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Desktop_App_01.ViewModels
+{
+    public partial class EditStudentWindowVM : ObservableObject
+    {
+        [ObservableProperty]
+        public string firstName;
+
+        [ObservableProperty]
+        public string lastName;
+
+        [ObservableProperty]
+        public int age;
+
+        [ObservableProperty]
+        public string image;
+
+        int index;
+
+        public EditStudentWindowVM()
+        {
+            FirstName = null;
+            LastName = null;
+            Age = 0;
+            Image = null;
+        }
+
+        public EditStudentWindowVM(int id, string fname, string lname, int a, string img)
+        {
+            index = id;
+            FirstName = fname;
+            LastName = lname;
+            Age = a;
+            Image = img;
+
+        }
+
+        [RelayCommand]
+        public void editStudent()
+        {
+            using (var db = new DataBaseContext())
+            {
+                var students = db.ListofStudents;
+                foreach (var s in students)
+                {
+                    if(index == s.ID)
+                    {
+                        s.FirstName = firstName;
+                        s.LastName = lastName;
+                        s.Age = age;
+                        s.Image = image;
+                    }
+                }
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Desktop_App_01/Views/EditStudentWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Desktop_App_01.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Desktop_App_01.Views
+{
+    /// <summary>
+    /// Interaction logic for EditStudentWindow.xaml
+    /// </summary>
+    public partial class EditStudentWindow : Window
+    {
+        public EditStudentWindow()
+        {
+            InitializeComponent();
+            DataContext = new EditStudentWindowVM();
+        }
+
+        public EditStudentWindow(int id, string fname, string lname, int age, string img)
+        {
+            InitializeComponent();
+            DataContext = new EditStudentWindowVM(id, fname, lname, age, img);
+        }
+
+        private void Button_Cancel(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+
+        private void Button_Edit(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
