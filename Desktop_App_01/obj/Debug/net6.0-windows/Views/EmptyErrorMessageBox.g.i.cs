6eb1e033e5484cda7915e1e38d67c15930df3bfd
Inupa Vihan <inupavihan9@gmail.com>
2023-07-15 22:21:57 +0530

    Finalized the Project
    -Finalized for submitting

--- Desktop_App_01/obj/Debug/net6.0-windows/Views/EmptyErrorMessageBox.g.i.cs
+++ Desktop_App_01/obj/Debug/net6.0-windows/Views/EmptyErrorMessageBox.g.i.cs
@@ -53,7 +53,7 @@ namespace Desktop_App_01.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Desktop_App_01;component/views/emptyerrormessagebox.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Desktop_App_01;V1.0.0.0;component/views/emptyerrormessagebox.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\EmptyErrorMessageBox.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
